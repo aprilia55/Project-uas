4f4ea58329712e7992f1c2edc5c766f4dc0a4f02
aprilia_lea <aprilliaputriofficial@gmail.com>
2025-01-07 12:02:40 +0700

    Update class_view.py

--- class_view.py
+++ class_view.py
@@ -1,6 +1,7 @@
 class View:
-    def display_table(self, data):
-        print(f"{'Name':<20}{'Age':<5}")
-        print("-" * 25)
-        for entry in data:
-            print(f"{entry['name']:<20}{entry['age']:<5}")
+    def display_data(self, data):
+        print("=================================")
+        print("| Nama | Usia                  |")
+        print("=================================")
+        print(f"| {data.get_name():<4} | {data.get_age():<20} |")
+        print("=================================")
